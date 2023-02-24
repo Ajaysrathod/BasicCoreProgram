bc5a0b5dd19726797e0b1a8d612cb61c2e7b4a29
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-02-24 21:29:02 +0530

    [Ajay]-SwappingTwoNumberProgram-

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to basic core program");
-            Console.WriteLine("1.FlipCoin\n2:Leap Year\n3:PowerOfTwo\n4:Harmonics");
+            Console.WriteLine("1.FlipCoin\n2:Leap Year\n3:PowerOfTwo\n4:Harmonics\n5:FindQuotient\n6:SwappingTwoNumber");
             Console.WriteLine("Enter a option");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -30,6 +30,11 @@
                     FindQuotient findQuotient = new FindQuotient();
                     findQuotient.FindQuotients();
                     break;
+                case 6:
+                    SwappingTwoNumber swappingTwoNumber = new SwappingTwoNumber();
+                    swappingTwoNumber.SwappingTwoNumbers();
+                    break;
+                
             }
         }
     }
---
+++ SwappingTwoNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class SwappingTwoNumber
+    {
+        public void SwappingTwoNumbers()
+        {
+            Console.WriteLine("Please enter number");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter number");
+            int j = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The number before swapping {0} and {1}", i, j);
+            int temp = i;
+            i = j;
+            Console.WriteLine("The number after Swapping {0} and {1}", i, j);
+        }
+    }
+}
