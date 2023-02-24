3c8999e9c4fd4aefb70d8ab1e7be6480cf3be6ac
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-02-24 17:28:57 +0530

    [Ajay]-FlipCoinsProgram-

---
+++ FlipCoin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class FlipCoin
+    {
+
+        float CountHead = 0;
+        float CountTail = 0;
+
+
+        public void FlipCoins()
+        {
+            Console.WriteLine("Enter no of times to flip coin");
+            int flipNum = Convert.ToInt32(Console.ReadLine());
+
+            Random random = new Random();
+            for (int i = 0; i < flipNum; i++)
+            {
+                double numCheck = random.NextDouble();
+                if (numCheck > 0.5)
+
+                    CountHead++;
+                else
+                    CountTail++;
+            }
+            Console.WriteLine("The HeadCount is " + CountHead);
+            Console.WriteLine("The HeadCount is" + CountTail);
+            float Headpercentage = (CountHead / flipNum) * 100;
+            float tailpercentage = (CountTail / flipNum) * 100;
+            Console.WriteLine("Head count percentage is:" + Headpercentage);
+            Console.WriteLine("Tail count percentage is:" + tailpercentage);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace BasicCoreProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to basic core program");
+            Console.WriteLine("Enter a option");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FlipCoin flipCoin = new FlipCoin();
+                    flipCoin.FlipCoins();
+                    break;
+            }
+        }
+    }
+}
