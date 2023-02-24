eb0f8f40628af05e5688396b3176a83a33b70dd2
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-02-24 22:06:30 +0530

    [Ajay]-FactorsProgram-

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to basic core program");
-            Console.WriteLine("1.FlipCoin\n2:Leap Year\n3:PowerOfTwo\n4:Harmonics\n5:FindQuotient\n6:SwappingTwoNumber\n7:EvenOrOdd\n8:ConsonantVowel\n9:LargestNumber");
+            Console.WriteLine("1.FlipCoin\n2:Leap Year\n3:PowerOfTwo\n4:Harmonics\n5:FindQuotient\n6:SwappingTwoNumber\n7:EvenOrOdd\n8:ConsonantVowel\n9:LargestNumber\n10:Factors");
             Console.WriteLine("Enter a option");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -45,6 +45,10 @@
                 case 9:
                     LargestNumber largestNumber = new LargestNumber();
                     largestNumber.LargestOfThreeNumber();
+                    break;
+                case 10:
+                    Factors factors = new Factors();
+                    factors.FactorsNumber();
                     break;
 
             }
