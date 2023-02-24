449749e7a7cf9c63e3f0e1db8eadeb988550199a
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-02-24 17:37:52 +0530

    [Ajay]-LeapYearProgram-

---
+++ LeapYear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class LeapYear
+    {
+        public void Leap()
+        {
+            Console.WriteLine("Enter the year you want to check for leap year");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine("The {0} is leap year", year);
+            }
+            else
+            {
+                Console.WriteLine("The {0} is not leap year", year);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to basic core program");
+            Console.WriteLine("1.FlipCoin\n2:Leap Year");
             Console.WriteLine("Enter a option");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -12,6 +13,10 @@
                 case 1:
                     FlipCoin flipCoin = new FlipCoin();
                     flipCoin.FlipCoins();
+                    break;
+                case 2:
+                    LeapYear leapYear = new LeapYear();
+                    leapYear.Leap();
                     break;
             }
         }
